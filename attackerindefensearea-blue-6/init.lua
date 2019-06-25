a374373ba9e755752c71564de51675ede792c51f
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2019-06-25 12:24:23 +0200

    Update for rule changes in attacker touch keeper etc.

--- attackerindefensearea-blue-6/init.lua
+++ attackerindefensearea-blue-6/init.lua
@@ -1,7 +1,7 @@
 local Helpers = require "../autoreftesthelpers"
 
 return Helpers.createReplayTestRunner(" main",
-	Helpers.expectEventMultiple("attacker_in_defense_area", {
+	Helpers.expectEventMultiple("attacker_touched_ball_in_defense_area", {
 		Helpers.expectTeam(false), -- blue
 		Helpers.expectBotId(6),
 		--Helpers.expectRoughLocation(0, 0)
--- attackertouchkeeper-yellow-14/init.lua
+++ attackertouchkeeper-yellow-14/init.lua
@@ -1,7 +1,7 @@
 local Helpers = require "../autoreftesthelpers"
 
 return Helpers.createReplayTestRunner(" main",
-	Helpers.expectEventMultiple("attacker_touched_keeper", {
+	Helpers.expectEventMultiple("attacker_touched_opponent_in_defense_area", {
 		Helpers.expectTeam(true), -- yellow
 		Helpers.expectBotId(14),
 		--Helpers.expectRoughLocation(0, 0)
